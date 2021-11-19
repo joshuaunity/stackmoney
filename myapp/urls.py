ad7b2408e43e5f76655b746e7ae5344718d77cd0
joshuaunity <oghenerobojosh01@gmail.com>
2021-11-19 22:50:00 +0100

    finsinhed create transactions

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('register', views.register, name='register'),
-    # path('login', views.login, name='login'),
-    # path('dashboard', views.dashboard, name='dashboard'),
-    # path('logout', views.logout, name='logout'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('logout', views.logout, name='logout'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('transaction.create', views.create_transaction, name='createtransaction'),
     # path('post/<str:title>', views.post, name='post'),
 ]
