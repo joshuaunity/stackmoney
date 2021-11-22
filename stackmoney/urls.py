6e4bb6e2e1d60bc8fe1a0bfe1971f162a5bdb79a
joshuaunity <oghenerobojosh01@gmail.com>
2021-11-22 15:27:34 +0100

    signup bug fix

--- stackmoney/urls.py
+++ stackmoney/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('', include('myapp.urls')),
     # path('api-auth/', include('rest_framework.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
